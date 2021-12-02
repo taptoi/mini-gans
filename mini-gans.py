53b1929fac7ddf3ee99a78ecdf19a29860d2ee18
unknown <toivasentapani@gmail.com>
2021-12-02 23:23:05 +0100

    Added weights & biases reporting and a CLI

--- mini-gans.py
+++ mini-gans.py
@@ -1,3 +1,5 @@
+import click
+import wandb
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -5,6 +7,8 @@ import torchvision
 import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
+import PIL.Image
+import numpy as np
 from torch.utils.tensorboard import SummaryWriter
 
 # Discriminator judges the image if it is a fake or real
@@ -37,71 +41,103 @@ class Generator(nn.Module):
     def forward(self, x):
         return self.gen(x)
 
-# Hyperparameters:
-device = "cuda" if torch.cuda.is_available() else "cpu"
-lr = 3e-4
-z_dim = 64
-image_dim = 28 * 28 * 1
-batch_size = 32
-num_epochs = 50
+@click.command()
+@click.option('--num_epochs', default=50, help='Number of epochs to train.')
+@click.option('--report_tensorboard', default=False, help='Use tensorboard for reporting.')
+@click.option('--report_wandb', default=False, help='Use weights & biases for reporting.')
+def train(num_epochs, report_tensorboard, report_wandb):
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    learning_rate = 3e-4
+    z_dim = 64
+    image_dim = 28 * 28 * 1
+    batch_size = 32
 
-disc = Discriminator(image_dim).to(device)
-gen = Generator(z_dim, image_dim).to(device)
-fixed_noise = torch.randn((batch_size, z_dim)).to(device)
-tforms = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5))]
-)
-dataset = datasets.MNIST(root="dataset/", transform=tforms, download=True)
-loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-opt_disc = optim.Adam(disc.parameters(), lr=lr)
-opt_gen = optim.Adam(gen.parameters(), lr=lr)
-criterion = nn.BCELoss()
-writer_fake = SummaryWriter(f"runs/GAN_MNIST/fake")
-writer_real = SummaryWriter(f"runs/GAN_MNIST/real")
-step = 0
+    disc = Discriminator(image_dim).to(device)
+    gen = Generator(z_dim, image_dim).to(device)
+    fixed_noise = torch.randn((batch_size, z_dim)).to(device)
+    tforms = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5,), (0.5))]
+    )
+    dataset = datasets.MNIST(root="dataset/", transform=tforms, download=True)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    opt_disc = optim.Adam(disc.parameters(), lr=learning_rate)
+    opt_gen = optim.Adam(gen.parameters(), lr=learning_rate)
+    criterion = nn.BCELoss()
+    writer_fake = SummaryWriter(f"runs/GAN_MNIST/fake")
+    writer_real = SummaryWriter(f"runs/GAN_MNIST/real")
 
-for epoch in range(num_epochs):
-    for batch_idx, (real, _) in enumerate(loader):
-        real = real.view(-1, 784).to(device)
-        batch_size = real.shape[0]
+    if(report_wandb):
+        wandb.init(project="mini-gans", entity="taptoi")
+        wandb.config = {
+                "learning_rate": learning_rate,
+                "epochs": num_epochs,
+                "batch_size": batch_size
+                }
 
-        ### Train Discruminator: max log(D(real)) 0 log(1 - D(G(z))
-        # sample minibatch of noise samples from noise prior p_g(z)
-        noise = torch.randn(batch_size, z_dim).to(device)
-        # sample minibatch of examples from data generating distribution
-        fake = gen(noise)
-        disc_real = disc(real).view(-1)
-        lossD_real = criterion(disc_real, torch.ones_like(disc_real))
-        disc_fake = disc(fake).view(-1)
-        lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
-        lossD = 0.5 * (lossD_real + lossD_fake) / 2
-        disc.zero_grad()
-        lossD.backward(retain_graph=True) # to preserve fake computation
-        opt_disc.step()
+    for epoch in range(num_epochs):
+        for batch_idx, (real, _) in enumerate(loader):
+            real = real.view(-1, 784).to(device)
+            batch_size = real.shape[0]
 
-        output = disc(fake).view(-1)
-        lossG = criterion(output, torch.ones_like(output))
-        gen.zero_grad()
-        lossG.backward()
-        opt_gen.step()
+            ### Train Discruminator: max log(D(real)) 0 log(1 - D(G(z))
+            # sample minibatch of noise samples from noise prior p_g(z)
+            noise = torch.randn(batch_size, z_dim).to(device)
+            # sample minibatch of examples from data generating distribution
+            fake = gen(noise)
+            disc_real = disc(real).view(-1)
+            lossD_real = criterion(disc_real, torch.ones_like(disc_real))
+            disc_fake = disc(fake).view(-1)
+            lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+            lossD = 0.5 * (lossD_real + lossD_fake) / 2
+            disc.zero_grad()
+            lossD.backward(retain_graph=True) # to preserve fake computation
+            opt_disc.step()
 
-        if batch_idx == 0:
-            print(
-                f"Epoxh [{epoch}/{num_epochs}] \ "
-                f"Loss D: {lossD:.4f}, Loss G: {lossG:.4f}"
-            )
+            output = disc(fake).view(-1)
+            lossG = criterion(output, torch.ones_like(output))
+            gen.zero_grad()
+            lossG.backward()
+            opt_gen.step()
 
-            with torch.no_grad():
-                fake = gen(fixed_noise).reshape(-1, 1, 28, 28)
-                data = real.reshape(-1, 1, 28, 28)
-                img_grid_fake = torchvision.utils.make_grid(fake, normalize=True)
-                img_grid_real = torchvision.utils.make_grid(data, normalize=True)
+            if(report_tensorboard):
+                if batch_idx == 0:
+                    print(
+                        f"Epoch [{epoch + 1}/{num_epochs}] \ "
+                        f"Loss D: {lossD:.4f}, Loss G: {lossG:.4f}"
+                    )
 
-                writer_fake.add_image(
-                    "Mnist fakes", img_grid_fake, global_step=step
-                )
-                writer_fake.add_image(
-                    "Mnist reals", img_grid_real, global_step=step
-                )
-                step += 1
+                    with torch.no_grad():
+                        fakes = gen(fixed_noise).reshape(-1, 1, 28, 28)
+                        reals = real.reshape(-1, 1, 28, 28)
+                        img_grid_fake = torchvision.utils.make_grid(fakes, normalize=True)
+                        img_grid_real = torchvision.utils.make_grid(reals, normalize=True)
 
+                        writer_fake.add_image(
+                            "Mnist fakes", img_grid_fake, global_step=epoch
+                        )
+                        writer_real.add_image(
+                            "Mnist reals", img_grid_real, global_step=epoch
+                        )
+                        
+            
+            if(report_wandb):
+                if batch_idx == 0:
+                    print(
+                        f"Epoch [{epoch + 1}/{num_epochs}] \ "
+                        f"Loss D: {lossD:.4f}, Loss G: {lossG:.4f}"
+                    )
+                    wandb.log({"lossD": lossD, 
+                                "lossG": lossG,
+                                "epoch": epoch,
+                                })
+                    fakes = gen(fixed_noise).reshape(-1, 1, 28, 28)
+                    reals = real.reshape(-1, 1, 28, 28)
+                    img_grid_fake = torchvision.utils.make_grid(fakes, normalize=True)
+                    img_grid_real = torchvision.utils.make_grid(reals, normalize=True)
+                    wdb_fakes = wandb.Image(img_grid_fake, caption="Fakes")
+                    wdb_reals = wandb.Image(img_grid_real, caption="Reals")
+                    wandb.log({"fakes": wdb_fakes})
+                    wandb.log({"reals": wdb_reals})
+
+if __name__ == '__main__':
+    train()
